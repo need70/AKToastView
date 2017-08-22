56b7d2ef2d3f77f418ea215eae639fd86f43a1f0
Andmar <andrew.kramar@yahoo.com>
2017-08-22 12:17:42 +0300

    Initial commit

--- AKToastViewExample/ViewController.swift
+++ AKToastViewExample/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showToastAction(_ sender: Any) {
+        showToast()
+        perform(#selector(hideToast), with: nil, afterDelay:3)
     }
-
-
+    
+    func showToast() {
+        AKToastView.show("Loading...")
+    }
+    
+    func hideToast() {
+        AKToastView.hide(completion: nil)
+    }
+    
+    //MARK: - TableView
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Cell #\(indexPath.row)"
+        
+        return cell
+    }
 }
 
